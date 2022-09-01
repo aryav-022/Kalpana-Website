67746053e4dea00e1f1512ba60f7aaf6dd517407
Aryav Bhola <aryavbhola@gmail.com>
2022-09-01 23:18:01 +0530

    Navbar, Routing, Fixed

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,18 +1,28 @@
 import React, { useEffect, useRef } from 'react';
 import styles from "./Navbar.module.css";
-
 import { Link } from "react-router-dom";
 import {BsFillMenuButtonWideFill} from 'react-icons/bs';
 import logo from '../../Assets/Logo/kalpanacaps.png';
 
-export default function index() {
-  
-  const Checking = useRef(null);
+export default function Navbar() {
 
+  const CheckBox = useRef(null);
+  const NavLinksContainer = useRef(null);
+  
   useEffect(() => {
-    const uncheck = () => {
-      Checking.current.checked = false;
-  };
+    const NavLinks = [...NavLinksContainer.current.children];
+    function uncheck() {
+      CheckBox.current.checked = false;
+    }
+    NavLinks.forEach(NavLink => {
+      NavLink.addEventListener('click', uncheck);
+    })
+    
+    return (() => {
+      NavLinks.forEach(NavLink => {
+        NavLink.removeEventListener('click', uncheck);
+      })
+    }) 
   })
   
   return (
@@ -22,17 +32,17 @@ export default function index() {
           <img src={logo} alt="logo" style={{width:'100px'}} />
             <p className={styles.nav_title}>Team Kalpana</p>
          </div>
-         <input ref={Checking} type="checkbox" id="click" className={styles.click} />
-         <label for="click" className={styles.menu_btn}>
-         {/* <i class="fas fa-bars"></i> */}
+         <input ref={CheckBox} type="checkbox" id="click" className={styles.click} />
+         <label htmlFor="click" className={styles.menu_btn}>
+         {/* <i className="fas fa-bars"></i> */}
           <BsFillMenuButtonWideFill className={styles.fas}/>
          </label>
-         <ul>
-            <li><Link to="/" onClick={uncheck}>Home</Link></li>
-            <li><Link to="/team" onClick={uncheck}>Team</Link></li>
-            <li><Link to="/achievements" onClick={uncheck}>Achievements</Link></li>
-            <li><Link to="/sponsors" onClick={uncheck}>Sponsors</Link></li>
-            <li><Link to="/blogs" onClick={uncheck}>Blogs</Link></li>
+         <ul ref={NavLinksContainer}>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/team">Team</Link></li>
+            <li><Link to="/achievements">Achievements</Link></li>
+            <li><Link to="/sponsors">Sponsors</Link></li>
+            <li><Link to="/blogs">Blogs</Link></li>
          </ul>
       </nav>
     </div>
