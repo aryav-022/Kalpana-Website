4e46942264138e47fe311b3d450c318f7f80a7a0
Hardchik <hardik.chhabra.ug20@nsut.ac.in>
2022-09-01 19:02:14 +0530

    few changes

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Footer.scss";
-import {FaInstagramSquare, FaFacebookSquare, FaYoutube, FaTwitter} from "react-icons/fa";
+import { Link } from "react-router-dom";
+import {FaInstagramSquare, FaFacebookSquare, FaYoutube, FaLinkedin} from "react-icons/fa";
 import logo from "../../Assets/Logo/kalpanacaps.png";
 
 export default function index() {
@@ -25,23 +26,21 @@ export default function index() {
           <ul class="footer-list-top">
             <li id='help'>
               <h4 class="footer-list-header">Site Map</h4></li>
-            <li><a href='/shop/about-contact' class="generic-anchor footer-list-anchor" itemprop="significantLink">HOME</a></li>
-            <li><a href='/faq.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">TEAM</a></li>
-            <li id='find-a-store'><a href='/shop/store-locator' class="generic-anchor footer-list-anchor" itemprop="significantLink">ACHIEVEMENTS</a></li>
-            <li id='user-registration'><a href='/shop/user-registration?URL=' class="generic-anchor footer-list-anchor" itemprop="significantLink">SPONSORS</a></li>
-            <li id='order-tracking'><a href='/shop/order-status' itemprop="significantLink" class="generic-anchor footer-list-anchor">BLOGS</a></li>
+            <li><Link to='/' class="generic-anchor footer-list-anchor" itemprop="significantLink">HOME</Link></li>
+            <li><Link to='/team' class="generic-anchor footer-list-anchor" itemprop="significantLink">TEAM</Link></li>
+            <li id='find-a-store'><Link to='/achievements' class="generic-anchor footer-list-anchor" itemprop="significantLink">ACHIEVEMENTS</Link></li>
+            <li id='user-registration'><Link to='/sponsors' class="generic-anchor footer-list-anchor" itemprop="significantLink">SPONSORS</Link></li>
+            <li id='order-tracking'><Link to='/blogs' itemprop="significantLink" class="generic-anchor footer-list-anchor">BLOGS</Link></li>
           </ul>
           <section class="footer-social-section flex-rw">
               <span class="footer-social-overlap footer-social-connect">
               CONNECT <span class="footer-social-small">with</span> US
               </span>
               <span class="footer-social-overlap footer-social-icons-wrapper">
-              {/* <a href="https://www.pinterest.com/paviliongift/" class="generic-anchor" target="_blank" title="Pinterest" itemprop="significantLink"><i class="fa fa-pinterest"></i></a> */}
-              <a href="https://www.facebook.com/paviliongift" class="generic-anchor" target="_blank" title="Facebook" itemprop="significantLink"><FaFacebookSquare /></a>
-              <a href="https://twitter.com/PavilionGiftCo" class="generic-anchor" target="_blank" title="Twitter" itemprop="significantLink"><FaTwitter /></a>
-              <a href="http://instagram.com/paviliongiftcompany" class="generic-anchor" target="_blank" title="Instagram" itemprop="significantLink"><FaInstagramSquare /></a>
+              <a href="https://www.facebook.com/Team-Kalpana-425579541226330/" class="generic-anchor" target="_blank" title="Facebook" itemprop="significantLink"><FaFacebookSquare /></a>
+              <a href="https://in.linkedin.com/company/team-kalpana" class="generic-anchor" target="_blank" title="Twitter" itemprop="significantLink"><FaLinkedin /></a>
+              <a href="https://www.instagram.com/teamkalpanansut/" class="generic-anchor" target="_blank" title="Instagram" itemprop="significantLink"><FaInstagramSquare /></a>
               <a href="https://www.youtube.com/channel/UCYgUODvd0qXbu_LkUWpTVEg" class="generic-anchor" target="_blank" title="Youtube" itemprop="significantLink"><FaYoutube /></a>
-              {/* <a href="https://plus.google.com/+Paviliongift/posts" class="generic-anchor" target="_blank" title="Google Plus" itemprop="significantLink"><i class="fa fa-google-plus"></i></a> */}
               </span>
           </section>
           <section class="footer-bottom-section flex-rw">
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import "./Navbar.css";
+import React, { useEffect, useRef } from 'react';
+import styles from "./Navbar.module.css";
+
+import { Link } from "react-router-dom";
 import {BsFillMenuButtonWideFill} from 'react-icons/bs';
 import logo from '../../Assets/Logo/kalpanacaps.png';
 
 export default function index() {
+  
+  const Checking = useRef(null);
+
+  useEffect(() => {
+    const uncheck = () => {
+      Checking.current.checked = false;
+  };
+  })
+  
   return (
-    // <div>
-    //   Navbar
-    // </div>
-    <div class="navb">
-      <nav>
-         <div class="logo" style={{display: 'flex', alignItems: 'center'}}>
+    <div className={styles.navb}>
+      <nav className={styles.navbar}>
+         <div className={styles.logo} style={{display: 'flex', alignItems: 'center'}}>
           <img src={logo} alt="logo" style={{width:'100px'}} />
-            <p class="nav-title">Team Kalpana</p>
+            <p className={styles.nav_title}>Team Kalpana</p>
          </div>
-         <input type="checkbox" id="click" />
-         <label for="click" class="menu-btn">
+         <input ref={Checking} type="checkbox" id="click" className={styles.click} />
+         <label for="click" className={styles.menu_btn}>
          {/* <i class="fas fa-bars"></i> */}
-         <BsFillMenuButtonWideFill class="fas fa-bars"/>
+          <BsFillMenuButtonWideFill className={styles.fas}/>
          </label>
          <ul>
-            <li><a class="active" href="#">Home</a></li>
-            <li><a href="#">Team</a></li>
-            <li><a href="#">Achievements</a></li>
-            <li><a href="#">Sponsors</a></li>
-            <li><a href="#">Blogs</a></li>
+            <li><Link to="/" onClick={uncheck}>Home</Link></li>
+            <li><Link to="/team" onClick={uncheck}>Team</Link></li>
+            <li><Link to="/achievements" onClick={uncheck}>Achievements</Link></li>
+            <li><Link to="/sponsors" onClick={uncheck}>Sponsors</Link></li>
+            <li><Link to="/blogs" onClick={uncheck}>Blogs</Link></li>
          </ul>
       </nav>
     </div>
