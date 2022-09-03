167cda0c3d0958f6f3b4dafa9cb9401e246a0790
pr0secutor <ifahim3107@gmail.com>
2022-09-04 01:40:36 +0530

    Made changes to Teams section

--- src/Pages/Team/Card.jsx
+++ src/Pages/Team/Card.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import styles from "./Team.module.css";
 
 const card = (props) => {
   return (
-    <a className="grid__item" href={props.href}>
-        <div className="box">
-            <div className="box__shadow"></div>
-            <img className="box__img" src={props.img} alt=""/>
-            <h3 className={props.box_title_css}><span className="box__title-inner" data-hover={props.box_title}>{props.box_title}</span></h3>
+    <a className={styles.grid__item} href={props.href}>
+        <div className={styles.box}>
+            <div className={styles.box__shadow}></div>
+            <img className={styles.box__img} src={props.img} alt=""/>
+            <h3 className={props.box_title_css}><span className={styles.box__title_inner} data-hover={props.box_title}>{props.box_title}</span></h3>
             <h4 className={props.box_text_css}><span className={props.box_text_inner_css}>{props.box_text}</span></h4>
             <div className={props.box_deco_css}>{props.box_deco}</div>
             {/* <p className="box__content">{props.box_tagline}</p> */}
--- src/Pages/Team/index.jsx
+++ src/Pages/Team/index.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
-import "./Team.css";
+import styles from "./Team.module.css";
 import Card from './Card'
 import Banner from '../Achievements/Banner';
+import Members from './Members-Lead.json'
 
-import img1 from '../../Assets/img/1.jpg'
-import img2 from '../../Assets/img/2.jpg'
-import img3 from '../../Assets/img/3.jpg'
-import img4 from '../../Assets/img/4.jpg'
-import img5 from '../../Assets/img/5.jpg'
-import img6 from '../../Assets/img/6.jpg'
+import img1 from '../../Assets/Images/img/1.jpg'
+import img2 from '../../Assets/Images/img/2.jpg'
+import img3 from '../../Assets/Images/img/3.jpg'
+import img4 from '../../Assets/Images/img/4.jpg'
+import img5 from '../../Assets/Images/img/5.jpg'
+import img6 from '../../Assets/Images/img/6.jpg'
 
 export default function index() {
   return (
     <>
       <Banner/>
-      <div className='content'>
-          <div className='team-head'><h1 >TEAM LEADS</h1></div>
-          <div className='grid'>
-              <Card
+      <div className={styles.content}>
+          <div className={styles.team_head}><h1 >TEAM LEADS</h1></div>
+          <div className={styles.grid}>
+            {Members.map(member => {
+              return(
+                <Card
+                  key={member.id}
+                  href={member.href}
+                  img={member.img}
+                  box_title={member.por}
+                  box_text={member.name}
+                  box_deco={member.box_deco}
+                  box_tagline={member.box_tagline}
+
+                  box_title_css={`${styles.box__title} ${styles.box__title__straight} ${styles.box__title__bottom}`}
+                  box_text_css={`${styles.box__text} ${styles.box__text__closer}`}
+                  box_deco_css={styles.box__deco}
+                  box_text_inner_css={styles.box__text_inner}
+                />
+              )
+            })}
+
+              {/* <Card
                   href="#preview-1"
                   img={img2}
                   box_title="Vice Pres."
@@ -25,10 +45,10 @@ export default function index() {
                   box_deco="&#10115;"
                   box_tagline=""
 
-                  box_title_css="box__title"
-                  box_text_css="box__text box__text--topcloser"
-                  box_deco_css="box__deco "
-                  box_text_inner_css="box__text-inner"
+                  box_title_css={styles.box__title}
+                  box_text_css={`${styles.box__text} ${styles.box__text__closer}`}
+                  box_deco_css={styles.box__deco}
+                  box_text_inner_css={styles.box__text_inner}
 
               />
 
@@ -40,31 +60,31 @@ export default function index() {
                 box_deco="&#10014;"
                 box_tagline=""
 
-                box_title_css="box__title box__title--straight box__title--bottom"
-                box_text_css="box__text"
-                box_deco_css="box__deco box__deco--top"
-                box_text_inner_css="box__text-inner"
+                box_title_css={`${styles.box__title} ${styles.box__title__straight} ${styles.box__title__bottom}`}
+                box_text_css={styles.box__text}
+                box_deco_css={`${styles.box__deco} ${styles.box__deco__top}`}
+                box_text_inner_css={styles.box__text_inner}
               />
 
               <Card
                   href="#preview-3"
-                    img={img3}
-                    box_title="Vice Pres."
-                    box_text="Tushar"
-                    box_deco="&#10032;"
-                    box_tagline="Call it a hurricane or call it freedom, Frank"
+                  img={img3}
+                  box_title="Vice Pres."
+                  box_text="Tushar"
+                  box_deco="&#10032;"
+                  box_tagline="Call it a hurricane or call it freedom, Frank"
 
-                    box_title_css="box__title"
-                    box_text_css="box__text"
-                    box_deco_css="box__deco"
-                    box_text_inner_css="box__text-inner"
-              />
+                  box_title_css={styles.box__title}
+                  box_text_css={styles.box__text}
+                  box_deco_css={styles.box__deco}
+                  box_text_inner_css={styles.box__text_inner}
+              /> */}
             </div>
       </div>
 
-      <div className='content'>
-            <div className='team-head'><h1 >UAS CHAPTER</h1></div>
-            <div className='grid'>
+      {/* <div className={styles.content}>
+          <div className={styles.team_head}><h1 >TEAM LEADS</h1></div>
+          <div className={styles.grid}>
                 <Card
                     href="#preview-4"
                     img={img4}
@@ -73,10 +93,10 @@ export default function index() {
                     box_deco="&#10014;"
                     box_tagline="Sometimes we go surfing when it's stormy weather"
 
-                    box_title_css="box__title"
-                    box_text_css="box__text"
-                    box_deco_css="box__deco"
-                    box_text_inner_css="box__text-inner"
+                    box_title_css={styles.box__title}
+                    box_text_css={styles.box__text}
+                    box_deco_css={styles.box__deco}
+                    box_text_inner_css={styles.box__text_inner}
                 />
 
                 <Card
@@ -87,10 +107,10 @@ export default function index() {
                     box_deco="&#10115;"
                     box_tagline=""
 
-                    box_title_css="box__title box__title--straight box__title--bottom"
-                    box_text_css="box__text"
-                    box_deco_css="box__deco box__deco--top"
-                    box_text_inner_css="box__text-inner"
+                    box_title_css={`${styles.box__title} ${styles.box__title__straight} ${styles.box__title__bottom}`}
+                    box_text_css={styles.box__text}
+                    box_deco_css={`${styles.box__deco} ${styles.box__deco__top}`}
+                    box_text_inner_css={styles.box__text_inner}
 
                 />
 
@@ -102,13 +122,13 @@ export default function index() {
                     box_deco="&#10032;"
                     box_tagline="Call it a hurricane or call it freedom, Frank"
 
-                    box_title_css="box__title"
-                    box_text_css="box__text box__text--topcloser"
-                    box_deco_css="box__deco"
-                    box_text_inner_css="box__text-inner"
+                    box_title_css={styles.box__title}
+                    box_text_css={`${styles.box__text} ${styles.box__text__topcloser}`}
+                    box_deco_css={styles.box__deco}
+                    box_text_inner_css={styles.box__text_inner}
                 />
             </div>
-      </div>
+      </div> */}
     </>
   )
 }
