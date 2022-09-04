75ee1a1794b2312e08426d7fb4ec181eb96a9a24
pr0secutor <ifahim3107@gmail.com>
2022-09-04 17:03:41 +0530

    Moved the memebrs data to json and changed image import to be more optimized

--- src/Pages/Team/index.jsx
+++ src/Pages/Team/index.jsx
@@ -30,49 +30,6 @@ export default function index() {
                 />
               )
             })}
-
-              {/* <Card
-                  href="#preview-1"
-                  // img={img2}
-                  box_title="Vice Pres."
-                  box_text="Vandit"
-                  box_deco="&#10115;"
-                  box_tagline=""
-
-                  box_title_css={styles.box__title}
-                  box_text_css={`${styles.box__text} ${styles.box__text__closer}`}
-                  box_deco_css={styles.box__deco}
-                  box_text_inner_css={styles.box__text_inner}
-
-              />
-
-              <Card
-                href="#preview-2"
-                // img={img1}
-                box_title="President"
-                box_text="Alankriti"
-                box_deco="&#10014;"
-                box_tagline=""
-
-                box_title_css={`${styles.box__title} ${styles.box__title__straight} ${styles.box__title__bottom}`}
-                box_text_css={styles.box__text}
-                box_deco_css={`${styles.box__deco} ${styles.box__deco__top}`}
-                box_text_inner_css={styles.box__text_inner}
-              />
-
-              <Card
-                  href="#preview-3"
-                  // img={img3}
-                  box_title="Vice Pres."
-                  box_text="Tushar"
-                  box_deco="&#10032;"
-                  box_tagline="Call it a hurricane or call it freedom, Frank"
-
-                  box_title_css={styles.box__title}
-                  box_text_css={styles.box__text}
-                  box_deco_css={styles.box__deco}
-                  box_text_inner_css={styles.box__text_inner}
-              /> */}
             </div>
       </div>
 
