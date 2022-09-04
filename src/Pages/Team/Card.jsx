893a62d5317452017559aa7b9664087bc5fb8f97
pr0secutor <ifahim3107@gmail.com>
2022-09-04 16:58:54 +0530

    Fixed image not importing

--- src/Pages/Team/Card.jsx
+++ src/Pages/Team/Card.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import styles from "./Team.module.css";
 
 const card = (props) => {
+  const img = require(`../../Assets/Images/img/${props.img}`);
   return (
     <a className={styles.grid__item} href={props.href}>
         <div className={styles.box}>
             <div className={styles.box__shadow}></div>
-            <img className={styles.box__img} src={props.img} alt=""/>
+            <img className={styles.box__img} src={img} alt=""/>
             <h3 className={props.box_title_css}><span className={styles.box__title_inner} data-hover={props.box_title}>{props.box_title}</span></h3>
             <h4 className={props.box_text_css}><span className={props.box_text_inner_css}>{props.box_text}</span></h4>
             <div className={props.box_deco_css}>{props.box_deco}</div>
--- src/Pages/Team/index.jsx
+++ src/Pages/Team/index.jsx
@@ -2,14 +2,8 @@ import React from 'react';
 import styles from "./Team.module.css";
 import Card from './Card'
 import Banner from '../Achievements/Banner';
-import Members from './Members-Lead.json'
-
-import img1 from '../../Assets/Images/img/1.jpg'
-import img2 from '../../Assets/Images/img/2.jpg'
-import img3 from '../../Assets/Images/img/3.jpg'
-import img4 from '../../Assets/Images/img/4.jpg'
-import img5 from '../../Assets/Images/img/5.jpg'
-import img6 from '../../Assets/Images/img/6.jpg'
+import Members_Lead from './Members-Lead.json'
+import Members_UAS from './Members_UAS.json'
 
 export default function index() {
   return (
@@ -18,7 +12,7 @@ export default function index() {
       <div className={styles.content}>
           <div className={styles.team_head}><h1 >TEAM LEADS</h1></div>
           <div className={styles.grid}>
-            {Members.map(member => {
+            {Members_Lead.map(member => {
               return(
                 <Card
                   key={member.id}
@@ -30,8 +24,8 @@ export default function index() {
                   box_tagline={member.box_tagline}
 
                   box_title_css={`${styles.box__title} ${styles.box__title__straight} ${styles.box__title__bottom}`}
-                  box_text_css={`${styles.box__text} ${styles.box__text__closer}`}
-                  box_deco_css={styles.box__deco}
+                  box_text_css={`${styles.box__text} ${styles.box__text__topcloser}`}
+                  box_deco_css={`${styles.box__deco} ${styles.box__deco__top}`}
                   box_text_inner_css={styles.box__text_inner}
                 />
               )
@@ -39,7 +33,7 @@ export default function index() {
 
               {/* <Card
                   href="#preview-1"
-                  img={img2}
+                  // img={img2}
                   box_title="Vice Pres."
                   box_text="Vandit"
                   box_deco="&#10115;"
@@ -54,7 +48,7 @@ export default function index() {
 
               <Card
                 href="#preview-2"
-                img={img1}
+                // img={img1}
                 box_title="President"
                 box_text="Alankriti"
                 box_deco="&#10014;"
@@ -68,7 +62,7 @@ export default function index() {
 
               <Card
                   href="#preview-3"
-                  img={img3}
+                  // img={img3}
                   box_title="Vice Pres."
                   box_text="Tushar"
                   box_deco="&#10032;"
@@ -82,53 +76,29 @@ export default function index() {
             </div>
       </div>
 
-      {/* <div className={styles.content}>
+      <div className={styles.content}>
           <div className={styles.team_head}><h1 >TEAM LEADS</h1></div>
-          <div className={styles.grid}>
+            <div className={styles.grid}>
+            {Members_UAS.map(member => {
+              return(
                 <Card
-                    href="#preview-4"
-                    img={img4}
-                    box_title="V. Chair"
-                    box_text="Udhay"
-                    box_deco="&#10014;"
-                    box_tagline="Sometimes we go surfing when it's stormy weather"
+                  key={member.id}
+                  href={member.href}
+                  img={member.img}
+                  box_title={member.por}
+                  box_text={member.name}
+                  box_deco={member.box_deco}
+                  box_tagline={member.box_tagline}
 
-                    box_title_css={styles.box__title}
-                    box_text_css={styles.box__text}
-                    box_deco_css={styles.box__deco}
-                    box_text_inner_css={styles.box__text_inner}
+                  box_title_css={`${styles.box__title} ${styles.box__title__straight} ${styles.box__title__bottom}`}
+                  box_text_css={`${styles.box__text} ${styles.box__text__topcloser}`}
+                  box_deco_css={`${styles.box__deco} ${styles.box__deco__top}`}
+                  box_text_inner_css={styles.box__text_inner}
                 />
-
-                <Card
-                    href="#preview-5"
-                    img={img5}
-                    box_title="Chairman"
-                    box_text="Rohan"
-                    box_deco="&#10115;"
-                    box_tagline=""
-
-                    box_title_css={`${styles.box__title} ${styles.box__title__straight} ${styles.box__title__bottom}`}
-                    box_text_css={styles.box__text}
-                    box_deco_css={`${styles.box__deco} ${styles.box__deco__top}`}
-                    box_text_inner_css={styles.box__text_inner}
-
-                />
-
-                <Card
-                    href="#preview-6"
-                    img={img6}
-                    box_title="V. Chair"
-                    box_text="Dhruv"
-                    box_deco="&#10032;"
-                    box_tagline="Call it a hurricane or call it freedom, Frank"
-
-                    box_title_css={styles.box__title}
-                    box_text_css={`${styles.box__text} ${styles.box__text__topcloser}`}
-                    box_deco_css={styles.box__deco}
-                    box_text_inner_css={styles.box__text_inner}
-                />
+              )
+            })}   
             </div>
-      </div> */}
+      </div>
     </>
   )
 }
