c03f1a1bbb673b0e33ee21ca24afe43f41a666bb
Sakshi-Roy <93031750+Paras-Roy@users.noreply.github.com>
2022-09-05 19:03:22 +0530

    Achievements  page completed

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,11 +6,11 @@ const Banner = (props) => {
             <div className="bannerContainer">
                 <div className="years">
                     {/*Pass a function "setActiveYear" which takes argument and modifies content based on year.*/}
-                    <a href="#2022" onClick={() => props.setActiveYear(2022)}>2022</a>
-                    <a href="#2021" onClick={() => props.setActiveYear(2021)}>2021</a>
-                    <a href="#2020" onClick={() => props.setActiveYear(2020)}>2020</a>
-                    <a href="#2019" onClick={() => props.setActiveYear(2019)}>2019</a>
-                    <a href="#2018" onClick={() => props.setActiveYear(2018)}>2018</a>
+                    <a href="#achSectionContainer" onClick={() => props.setActiveYear(2022)} id="2022">2022</a>
+                    <a href="#achSectionContainer" onClick={() => props.setActiveYear(2021)}>2021</a>
+                    <a href="#achSectionContainer" onClick={() => props.setActiveYear(2020)}>2020</a>
+                    <a href="#achSectionContainer" onClick={() => props.setActiveYear(2019)}>2019</a>
+                    <a href="#achSectionContainer" onClick={() => props.setActiveYear(2018)}>2018</a>
                 </div>
                 <div className="bannerContent">
                     <h3>TEAM KALPANA</h3>
--- src/Pages/Achievements/AchievementSection.jsx
+++ src/Pages/Achievements/AchievementSection.jsx
@@ -1,20 +1,27 @@
 import Banner from "../../Components/Banner/Banner";
 import { useState } from "react";
 import achievements from './Achievements.json'
+import AchievementCard from "./AchievementCard";
 
 const AchievementSection = () => {
 
-    const [Achievements, setAchievements] = useState(achievements);
+    const [Achievements, setAchievements] = useState(achievements.filter(achievement => achievement.year === 2022));
 
     const setActiveYear = (year) => {
         const newAchievements = achievements.filter(Achievement => Achievement.year === year);
         setAchievements(newAchievements);
+        document.getElementById(2022).style.color = ' var(--tertiary-text-color)';
+        if(year==2022) {document.getElementById(2022).style.color = ' var(--primary-text-color)';}
     }
 
     return (
         <div>
            <Banner title="Our Achievements" setActiveYear = {setActiveYear}/>
-           
+           <div id="achSectionContainer">
+            <section id="achSection">
+                {Achievements.map(Achievement => <AchievementCard Achievement = {Achievement} key = {Achievement.id} img = {Achievement.coverphoto}/>)}
+           </section>
+           </div>
         </div>
     );
 }
