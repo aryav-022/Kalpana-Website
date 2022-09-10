21d54dd4e537e1485fa5a47c9b21230f12787f86
Aryav Bhola <aryavbhola@gmail.com>
2022-09-10 17:36:41 +0530

    Revert "blogs article page"
    This reverts commit 88933f546ad498c9d26e6956511c1146239c43a7.

--- src/Pages/Blogs/BlogArticle.jsx
+++ src/Pages/Blogs/BlogArticle.jsx
@@ -1,21 +1,9 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import styles from "./Blogs.module.css";
-import blogs from "./Blogs.json";
+import React from 'react'
 
 export default function BlogArticle() {
-  const { id } = useParams();
-  const blog = blogs.filter(blog => {return blog.id.toString() === id})[0];
-
   return (
-    <section className={styles.blogArticleSection}>
-      <img src={blog.image} alt="" className={styles.blogImage} />
-      <div className={styles.blogContainer}>
-        <div className={styles.blogHeading}>{blog.heading}</div>
-        <div className="bottomRule"></div>
-        <div className={styles.blogDate}>{blog.date}</div>
-        <div className={styles.blogContent}>{blog.content}</div>
-      </div>
-    </section>
+    <div>
+      
+    </div>
   )
 }
