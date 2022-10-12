3b11819d7745c72090de8158d7443d2576cbe222
Aryav Bhola <aryavbhola@gmail.com>
2022-10-12 14:47:09 +0530

    Optimizing useFetch Hook

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,13 +2,18 @@ import { useState, useEffect } from 'react'
 
 export function useFetch(api, init) {
     const [data, setData] = useState(init);
-
+    
     useEffect(() => {
-        fetch(api)
+        const controller = new AbortController();
+        fetch(api, { signal: controller.signal })
         .then(response => {
             return response.json();
         })
         .then(rsp => setData(rsp))
+
+        return () => {
+            controller.abort();
+        }
     }, [api])
 
     return data;
