74c761d98782ee81251e40181c96fccb274a8da7
Aryav Bhola <aryavbhola@gmail.com>
2022-10-18 17:20:54 +0530

    Firebase Hosting

--- src/Pages/Blogs/BlogArticle.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function BlogArticle() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- src/Pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import Home from "./Home";
-import Team from "./Team";
-import Achievements from "./Achievements";
-import Blogs from "./Blogs";
-import Sponsors from "./Sponsors";
-
-export { Home, Team, Achievements, Blogs, Sponsors };
